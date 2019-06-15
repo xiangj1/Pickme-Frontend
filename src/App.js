44d3598444547584ad158216aa6a5fe38e513a4f
Steven <xiangj1@outlook.com>
2019-06-14 21:56:22 -0700

    basic image upload & list succeed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import  firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/storage';
+
+import firebaseConfig from './config/FirebaseConfig';
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth().signInWithEmailAndPassword(firebaseConfig.email, firebaseConfig.password)
+.then(res => {
+  console.log('signing successfully');
+}).catch((err) => {
+    console.log(err);
+})
+
+// firebase.auth().signOut().then((res) => {
+//   console.log('signed out', res);
+// }).catch((err) => {
+//   console.log('!Signed out', err);
+// })
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  
+  handleChange(event) {
+    const files = Array.from(event.target.files);
+    this.setState({files});
+  }
+
+  //upload multi
+  fileUploadHandler = () => {
+    const storageRef = firebase.storage().ref();
+
+    this.state.files.forEach((file) => {
+      storageRef.child(`images/${file.name}`).put(file, {'contentType': file.type})
+      .then(async (snapshot) => {
+        console.log('Success', await snapshot.ref.getDownloadURL());
+      }).catch(err => {
+        console.log('Fail', file.name);
+      })
+    }) 
+  }
+
+  foo = () => {
+    const storageRef = firebase.storage().ref();
+
+    storageRef.child('/images').listAll().then((res) => {
+      console.log(res);
+
+      res.items.forEach((item) => {
+        item.getDownloadURL().then((res) => {
+          console.log(res);
+        })
+      })
+    }).catch(err => console.log(err));
+  }
+
+  render() {
+      return (
+          <div className="App">
+            <input id="file" type="file" onChange={this.handleChange.bind(this)} required multiple />
+            <button onClick={this.fileUploadHandler}>Upload!</button>
+            <button onClick={this.foo}>Show</button>            
+          </div>
+      )
+  }
 }
 
 export default App;
