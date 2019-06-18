4d2a897aaf08148b3b9360b5bc60c1d2a09483e6
Steven <xiangj1@outlook.com>
2019-06-18 13:30:22 -0700

    ImageUploader worked

--- src/components/ImageUploader.js
+++ src/components/ImageUploader.js
@@ -4,18 +4,37 @@ import firebase from '../services/Firebase';
 class ImageUploader extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      code: '',
+      files: [],
+    };
   }
 
   handleChange(event) {
     const files = Array.from(event.target.files);
-    this.setState({files});
+
+    const code = this.state.code;
+
+    this.setState({files, code});
+  }
+
+  handleCode = (event) => {
+    const files = this.state.files;
+
+    const code = event.target.value;
+
+    this.setState({files, code});
   }
 
   fileUploadHandler = async (event) => {
     try {
-      if (!this.state.files) {
+      if (!this.state.files || !this.state.files.length) {
         alert('Please Select file');
+        return;
+      }
+
+      if (!this.state.code || !this.state.code.length) {
+        alert('Please enter code');
         return;
       }
 
@@ -33,7 +52,7 @@ class ImageUploader extends React.Component {
               snapshot.ref.getDownloadURL()
                 .then(url => {
                   let [file_name] = file.name.split('.');
-                  databaseRef.ref('images/' + file_name).set({ file_name: file.name, url, selected: false });
+                  databaseRef.ref(`images/${this.state.code}/` + file_name).set({ file_name: file.name, url, selected: false });
 
                   resolve({ file_name: file.name, url })
 
@@ -55,6 +74,7 @@ class ImageUploader extends React.Component {
   render() {
     return (
       <div>
+        <input type="text" onChange={this.handleCode} />
         <input id="file" type="file" onChange={this.handleChange.bind(this)} required multiple />
         <button onClick={this.fileUploadHandler}>Upload!</button>
       </div>
