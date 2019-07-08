2f03fd62ddd00ee8f8523464d814fd1ed6d7aea2
Steven <xiangj1@outlook.com>
2019-07-07 21:18:54 -0700

    added Navbar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 
 import Gallery from './components/Gallery';
 import ImageUploader from './components/ImageUploader';
+import Navbar from './components/Navbar';
 
 class App extends React.Component {
   render() {
       return (
-          <div className="col-12">
-            <br></br>
-            <ImageUploader />
-            <Gallery />
+          <div>
+            <Navbar />
+            <div className='col-12'>
+              <ImageUploader />
+              <Gallery />
+            </div>
           </div>
       )
   }
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../images/android-chrome-512x512.png'
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div/>
+  }
+}
+
+export default Navbar;
